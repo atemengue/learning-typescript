e36ea6b17cc252b71f5a83822de7c5af350696b6
atemengue <moafembe@gmail.com>
2021-09-22 19:44:35 +0100

    add hooks

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,11 +1,23 @@
 /** @format */
 
-const RepositoriesList = () => {
+import { useState } from 'react';
+import { useActions } from '../hooks/useActions';
+
+/** @format */
+
+const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('');
+  const { searchRepositories } = useActions();
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement> | undefined) => {
+    event?.preventDefault();
+    searchRepositories(term);
+  };
   return (
     <div>
       <div>
-        <form>
-          <input />
+        <form onSubmit={onSubmit}>
+          <input onChange={(e) => setTerm(e.target.value)} />
           <button>Search</button>
         </form>
       </div>

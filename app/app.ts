fe3f7b06957f19fe075d186d0eea43d5bb705a0e
= <=>
2023-01-24 08:58:37 +0100

    adding annotations

--- app/app.ts
+++ app/app.ts
@@ -3,18 +3,31 @@
 function startGame() {
   // start a new game init
 
-  let playName: string = 'Regis';
+  let playName: string | undefined = getInputValue('playername');
   logPlayer(playName);
 
-  let messagesElement: HTMLElement | string =
-    document.getElementById('messages');
-
-  messagesElement = document.getElementById('messages');
-  messagesElement!.innerHTML = 'Welcomme to game! Start new Game';
+  postScore(80, playName);
 }
 
-function logPlayer(name) {
+function logPlayer(name: string = 'MultiMath Player'): void {
   console.log(`New game starting for player: ${name}`);
 }
 
+function getInputValue(elementID: string): string | undefined {
+  const inputElement: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById(elementID)
+  );
+  console.log(inputElement);
+  if (inputElement.value === '') {
+    return undefined;
+  } else {
+    return inputElement.value;
+  }
+}
+
+function postScore(score: number, playName: string = 'MultiMath Player'): void {
+  const scoreElement: HTMLElement | null =
+    document.getElementById('postedScores');
+  scoreElement!.innerHTML = `${score} - ${playName}`;
+}
 document.getElementById('startGame')!.addEventListener('click', startGame);
--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
 function startGame() {
-    let playName = 'Regis';
+    let playName = getInputValue('playername');
     logPlayer(playName);
-    let messagesElement = document.getElementById('messages');
-    messagesElement = document.getElementById('messages');
-    messagesElement.innerHTML = 'Welcomme to game! Start new Game';
+    postScore(80, playName);
 }
-function logPlayer(name) {
+function logPlayer(name = 'MultiMath Player') {
     console.log(`New game starting for player: ${name}`);
+}
+function getInputValue(elementID) {
+    const inputElement = (document.getElementById(elementID));
+    console.log(inputElement);
+    if (inputElement.value === '') {
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+}
+function postScore(score, playName = 'MultiMath Player') {
+    const scoreElement = document.getElementById('postedScores');
+    scoreElement.innerHTML = `${score} - ${playName}`;
 }
 document.getElementById('startGame').addEventListener('click', startGame);

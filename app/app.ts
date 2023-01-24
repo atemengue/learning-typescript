ebb11af8f6bfd6e200221f8134d573587627637e
= <=>
2023-01-24 08:31:47 +0100

    use type and annotations

--- app/app.ts
+++ app/app.ts
@@ -2,8 +2,19 @@
 
 function startGame() {
   // start a new game init
-  let messagesElement = document.getElementById('messages');
+
+  let playName: string = 'Regis';
+  logPlayer(playName);
+
+  let messagesElement: HTMLElement | string =
+    document.getElementById('messages');
+
+  messagesElement = document.getElementById('messages');
   messagesElement!.innerHTML = 'Welcomme to game! Start new Game';
+}
+
+function logPlayer(name) {
+  console.log(`New game starting for player: ${name}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
-"use strict";
 function startGame() {
+    let playName = 'Regis';
+    logPlayer(playName);
     let messagesElement = document.getElementById('messages');
+    messagesElement = document.getElementById('messages');
     messagesElement.innerHTML = 'Welcomme to game! Start new Game';
+}
+function logPlayer(name) {
+    console.log(`New game starting for player: ${name}`);
 }
 document.getElementById('startGame').addEventListener('click', startGame);
